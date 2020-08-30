10a42fa4c3fa1805cb5eeb2a1e7f5e2e9cde8489
Nikhi <nikhibhambra@gmail.com>
2020-08-30 15:03:57 -0400

    react-router initalizations

--- src/App.js
+++ src/App.js
@@ -1,55 +1,34 @@
 import React from 'react';
-import { Layout } from './AppStyles.js';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
-// pages
-import Header from './pages/homepage/header'
-import Landing from './pages/homepage/landing'
-import Map from './pages/homepage/map'
-import Timeline from './pages/homepage/timeline'
-import SchoolsList from './pages/homepage/schoolsList'
-import Footer from './pages/homepage/footer'
+// Top-Level Pages
+import Homepage from './pages/homepage'
+import InstitutionInformation from './pages/institutionInfo'
+import Submission from './pages/submission'
+import About from './pages/about'
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path=''>
-        <Homepage />
+      <Route path='/school/:schoolCode?'>
+        <InstitutionInformation />
       </Route>
     </Switch>
     <Switch>
-      <Route path=''>
-        <Homepage />
+      <Route path='/submit'>
+        <Submission />
       </Route>
     </Switch>
     <Switch>
-      <Route path=''>
-        <Homepage />
+      <Route path='/about'>
+        <About />
       </Route>
     </Switch>
     <Switch>
-      <Route path=''>
+      <Route>
         <Homepage />
       </Route>
     </Switch>
-    <Switch>
-      <Route path=''>
-        <Homepage />
-      </Route>
-    </Switch>
-    <Switch>
-      <Route path=''>
-        <Homepage />
-      </Route>
-    </Switch>
-    {/* <Layout>
-      <Header/>
-      <Landing/>
-      <Map id="map" />
-      <Timeline id="timeline"/>
-      <SchoolsList id="list"/>
-      <Footer/> 
-    </Layout> */}
   </Router>
 )
 
---
+++ src/pages/homepage/homepage.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// page components
+import Header from './header'
+import Landing from './landing'
+import Map from './map'
+import Timeline from './timeline'
+import SchoolsList from './schoolsList'
+import Footer from './footer'
+
+const Homepage = () => (
+<Layout>
+    <Header/>
+    <Landing/>
+    <Map id="map" />
+    <Timeline id="timeline"/>
+    <SchoolsList id="list"/>
+    <Footer/> 
+</Layout>
+)
+
+export default Homepage
